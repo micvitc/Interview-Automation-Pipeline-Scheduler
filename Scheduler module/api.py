a756a29217a2b3416f0cc5dfca8e74963727521b
S Gopi <gopi.s2020a@vitstudent.ac.in>
2022-02-07 01:01:25 +0530

    Updated scheduler.py and api.py
    Completely restructured input and output data formats

--- Scheduler module/api.py
+++ Scheduler module/api.py
@@ -5,27 +5,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import scheduler as sch
 
+# rename "app" to "schedulerApp" later
 app = FastAPI()
 
 class Item(BaseModel):
-    regNo: str
-    name: str
-    course: str
-    year: int
+    id: str
     date: date
     startTime: time
     endTime: time
 
 class Items(BaseModel):
-    candidates: List[Item]
+    data: List[Item]
+    length: int
 
 @app.get("/schedule/duration={duration}_break={brk}_start-date={startDate}_start-time={startTime}_end-time={endTime}", response_model=Items)
 async def get_schedule(duration, brk, startDate, startTime, endTime):
-    sch.callScheduler(duration, brk, startDate, startTime, endTime)
-    scheduledData = []
-    for key,val in sch.candidates.items():
-        val["regNo"] = key
-        scheduledData.append(val)
-    return {"candidates": scheduledData}
+    return sch.callScheduler(duration, brk, startDate, startTime, endTime)
 
 # example - http://127.0.0.1:8000/schedule/duration=17_break=5_start-date=2022-01-25_start-time=17:00_end-time=18:00
--- Scheduler module/scheduler.py
+++ Scheduler module/scheduler.py
@@ -5,64 +5,72 @@ This is the main scheduler program
 from datetime import datetime, timedelta, date, time
 
 # function to schedule interviews after collecting basic info
-def scheduler(duration, brk, currDate, startTime, endTime):
-    currTime = startTime
-    for rollNo in candidates.keys():
-        candidates[rollNo]["date"] = currDate.date()
-        candidates[rollNo]["startTime"] = currTime.time()
+def scheduler(duration, brk, currDate, currTime, endTime):
+    for id in input["ids"]:
+        candidate = dict()
+        candidate["id"] = id
+        candidate["date"] = currDate.date()
+        candidate["startTime"] = currTime.time()
         currTime += timedelta(minutes = duration)
-        candidates[rollNo]["endTime"] = currTime.time()
+        candidate["endTime"] = currTime.time()
         currTime += timedelta(minutes = brk)
+        output["data"].append(candidate)
 
         # checking if endTime is breached
         if (currTime.hour >= endTime.hour):
-            currTime = startTime
+            currTime = input["startTime"]
             currDate += timedelta(days = 1)
     
-    '''
-    # displaying scheduled timing
-    for rollNo in candidates.keys():
-        print(candidates[rollNo])
-    '''
-    
+    output["length"] = len(output["data"])
+
     # returning scheduled data
-    return candidates
+    return output
 
 # raw data got from backend
-candidates = {
-            "20bce1800":{"name":"Raghu", "course":"CSE", "year":2020},
-            "20bce1802":{"name":"Arvind", "course":"MECH", "year":2020},
-            "20bce1803":{"name":"Vaishnavi", "course":"CSE", "year":2021},
-            "20bce1804":{"name":"Gopal", "course":"ECE", "year":2022},
-            "20bce1805":{"name":"Shivam", "course":"EEE", "year":2021}
-            }
+input = {
+            "ids":["20bce1800","20bce1802","20bce1803","20bce1804","20bce1805"],
+            "duration": 0,
+            "brk": 0, 
+            "startDate": 0,
+            "startTime": 0,
+            "endTime": 0 
+        }
+
+output = {
+            "data": [],
+            "length": 0
+         }
 
 '''
-# function to collect basic info and call the scheduler from within this file
-def callScheduler():
-    # collecting basic info of interview schedule
-    duration = int(input("Enter durationation of 1 interview (in minutes): "))
-    brk = int(input("Enter durationation of break after 1 interview (in minutes): "))
-    startDate = datetime.fromisoformat(input("Enter starting day of interviews (YYYY-MM-DD): "))
-    startTime = datetime.combine(date(1,1,1), time.fromisoformat(input("Enter starting time of interviews everyday (HH:MM) (24 Hr Format): ")))
-    endTime = datetime.combine(date(1,1,1), time.fromisoformat(input("Enter ending time of interviews everyday (HH:MM) (24 Hr Format): ")))
+# required code to call scheduler from within "scheduler.py" for debugging purposes
 
-    # calling scheduler
-    scheduler(duration, brk, startDate, startTime, endTime)
-callScheduler()
+# raw data got from backend
+input = {
+            "ids":["20bce1800","20bce1802","20bce1803","20bce1804","20bce1805"],
+            "duration": 15, # (in minutes)
+            "brk": 5, # (in minutes)
+            "startDate": datetime.fromisoformat('2022-02-06'), # (YYYY-MM-DD)
+            "startTime": datetime.combine(date(1,1,1), time.fromisoformat("17:00")), # (HH:MM) (24 Hr Format)
+            "endTime": datetime.combine(date(1,1,1), time.fromisoformat("18:00")) # (HH:MM) (24 Hr Format)
+        }
+
+# calling scheduler
+scheduler(input["duration"], input["brk"], input["startDate"], input["startTime"], input["endTime"])
 '''
 
 # function to collect basic info from api.py and call the scheduler 
 def callScheduler(duration, brk, startDate, startTime, endTime):
     # collecting basic info of interview schedule
-    duration = int(duration)
-    brk = int(brk)
-    startDate = datetime.fromisoformat(startDate)
-    startTime = datetime.combine(date(1,1,1), time.fromisoformat(startTime))
-    endTime = datetime.combine(date(1,1,1), time.fromisoformat(endTime))
+    input["duration"] = int(duration)
+    input["brk"] = int(brk)
+    input["startDate"] = datetime.fromisoformat(startDate)
+    input["startTime"] = datetime.combine(date(1,1,1), time.fromisoformat(startTime))
+    input["endTime"] = datetime.combine(date(1,1,1), time.fromisoformat(endTime))
 
     # calling scheduler
-    return scheduler(duration, brk, startDate, startTime, endTime)
+    return scheduler(input["duration"], input["brk"], input["startDate"], input["startTime"], input["endTime"])
 
-
-
+# datetime object is used even for just date and time bcoz 
+# timedelta fn can be used only on datetime objects, 
+# but while returning, datetime is converted to 
+# date & time objects separately and returned
